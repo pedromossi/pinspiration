7ad52f84b9ea4b7dbd842114171aa4e704fe2b45
pedromossi <146216012+pedromossi@users.noreply.github.com>
2023-11-29 00:47:29 +0000

    before users 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@
 #  bio                    :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  image                  :string
+#  name                   :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
